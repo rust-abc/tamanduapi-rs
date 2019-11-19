bfb15c7588a8346dca564b1aa9e0794e4afaf08d
GrayJack <gr41.j4ck@gmail.com>
2019-11-19 13:33:25 -0300

    More data structures and more debugging

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use futures::executor::block_on;
 // use reqwest;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if let Ok(html) = block_on(ru::get_ru()) {
-        println!("{}", html);
+    if let Ok(html) = block_on(ru::get_table()) {
+        // println!("{}", html);
     };
 
     Ok(())
--- src/ru.rs
+++ src/ru.rs
@@ -1,8 +1,37 @@
+use std::collections::HashMap;
+
 use surf;
 use select::{document::Document, predicate::{Predicate, Attr, Class, Name}};
 // use reqwest;
 
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Days {
+    Seg,
+    Ter,
+    Qua,
+    Qui,
+    Sex,
+    Sab,
+    Dom,
+}
+
+#[derive(Debug, Default, PartialEq, PartialOrd, Hash)]
+pub struct Food {
+    main_course: String,
+    veggie: String,
+    extra: String,
+    salads: String,
+    dessert: String,
+    today: bool,
+}
+
+impl Food {
+
+}
+
 pub async fn get_table() -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
+    use Days::*;
+
     let doc = surf::get(
         "http://proap.ufabc.edu.br/nutricao-e-restaurantes-universitarios/cardapio-semanal",
     )
@@ -13,10 +42,30 @@ pub async fn get_table() -> Result<String, Box<dyn std::error::Error + Send + Sy
 
     let html = Document::from(doc.as_str());
     let card_semanal = html.find(Class("cardapio-semanal")).nth(0).unwrap();
-    let table = card_semanal.find(Name("table")).nth(0).unwrap();
+    let html_table = card_semanal.find(Name("table")).nth(0).unwrap();
 
-    for tr in table.find(Name("tr")) {
+    let mut table: HashMap<Days, HashMap<&str, Food>> = HashMap::new();
+    for tr in html_table.find(Name("tr")) {
+        match tr.attr("class") {
+            Some("cardapio-hoje") => {
+                let day_str = tr.find(Name("td")).nth(0).unwrap().text();
+                let day = match day_str {
+                    s if day_str.contains("Seg") => Seg,
+                    s if day_str.contains("Ter") => Ter,
+                    s if day_str.contains("Qua") => Qua,
+                    s if day_str.contains("Qui") => Qui,
+                    s if day_str.contains("Sex") => Sex,
+                    s if day_str.contains("Sab") => Sab,
+                    _ => Dom,
+                };
 
+                dbg!(tr.prev(), tr.next());
+
+                // table.insert(day, v: V);
+            },
+            Some(_) => {},
+            None => {}
+        }
     }
 
 
